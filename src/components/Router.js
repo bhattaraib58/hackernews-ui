5a2f74f2ea725156f0c4e55458ca5a158c757051
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-03-15 18:02:42 +0545

    Add proper route constants

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Router as BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import history from 'utils/history';
+import ROUTES from 'constants/routes';
 
 import Header from 'components/Common/Header';
 import Footer from 'components/Common/Footer';
 
-// import PageView from 'components/Pages/pageView';
-// import CommentView from 'components/Pages/commentView';
+import PageView from 'components/Pages/PageView';
 import PageNotFound from 'components/Pages/PageNotFound';
+// import CommentView from 'components/Pages/commentView';
 
 /**
  * Top level application router.
@@ -18,18 +19,16 @@ import PageNotFound from 'components/Pages/PageNotFound';
 const Router = () => {
   return (
     <BrowserRouter history={history}>
-      <Header />
+      <div className="body-container mt-10 mb-10">
+        <Header />
 
-      <div className="page-content news-grid">
         <Switch>
-          {/* <Route exact path="/" component={PageView} /> */}
-          {/* <Route path="/top" component={PageView} /> */}
-          {/* <Route path="/best" component={PageView} /> */}
-          {/* <Route path="/new" component={PageView} /> */}
+          <Route exact path={[ROUTES.HOME, ROUTES.NEWS_TYPE, ROUTES.NEWS_TYPE_WITH_PAGE]} component={PageView} />
           {/* <Route path="/item/:id" component={CommentView} /> */}
 
           <Route path="/" component={PageNotFound} />
         </Switch>
+
         <Footer />
       </div>
     </BrowserRouter>
---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+const NEWS_TYPE = '/:newsType';
+const NEWS_TYPE_WITH_PAGE = '/:newsType/:page';
+
+const ROUTES = {
+  HOME: '/',
+  NEWS_TYPE,
+  NEWS_TYPE_WITH_PAGE
+};
+
+export default ROUTES;
