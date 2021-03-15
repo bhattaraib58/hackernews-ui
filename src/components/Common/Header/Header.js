cae3b189769f4c579927bd4212d50c00c4c740af
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-03-15 18:01:26 +0545

    Fix header style

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from 'assets/images/y18.gif';
+import ROUTES from 'constants/routes';
+import Logo from 'assets/images/Logo.gif';
+
+import styles from './Header.module.css';
 
 /**
  * Header Component.
@@ -10,40 +13,42 @@ import Logo from 'assets/images/y18.gif';
  */
 function Header() {
   return (
-    <div className="page-content">
-      <div className="header clearfix">
-        <div className="logo">
-          <Link to="/">
+    <div className={`align-center ${styles.container}`}>
+      <div className="align-center mr-10">
+        <div className={styles.logo}>
+          <Link to={ROUTES.HOME}>
             <img src={Logo} alt="Hacker News" />
           </Link>
         </div>
-        <div className="logo-name">
+
+        <div className={styles.siteName}>
           <Link to="/top">Hacker News</Link>
         </div>
-        <ul className="links">
-          <li>
-            <Link to="/new">new</Link>
-          </li>
-          <li>
-            <Link to="/best">best</Link>
-          </li>
-          <li>
-            <Link to="/comments">comments</Link>
-          </li>
-          <li>
-            <Link to="/ask">ask</Link>
-          </li>
-          <li>
-            <Link to="/show">show</Link>
-          </li>
-          <li>
-            <Link to="/jobs">jobs</Link>
-          </li>
-          <li>
-            <Link to="/submit">submit</Link>
-          </li>
-        </ul>
       </div>
+
+      <ul className={`align-center ${styles.links}`}>
+        <li>
+          <Link to="/new">new</Link>
+        </li>
+        <li>
+          <Link to="/best">best</Link>
+        </li>
+        <li>
+          <Link to="/comments">comments</Link>
+        </li>
+        <li>
+          <Link to="/ask">ask</Link>
+        </li>
+        <li>
+          <Link to="/show">show</Link>
+        </li>
+        <li>
+          <Link to="/jobs">jobs</Link>
+        </li>
+        <li>
+          <Link to="/submit">submit</Link>
+        </li>
+      </ul>
     </div>
   );
 }
