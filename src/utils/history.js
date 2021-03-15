576e65fd7426bf0927dd2cc60703553b7f80828b
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-03-15 17:57:07 +0545

    Add route change auto scroll

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,3 +1,11 @@
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory();
+const history = createBrowserHistory();
+
+// this listens for a route change, and scrolls to the  top of the screen each time it changes
+// the back/forward buttons will push and pop, but will not hit this listener & scroll to the top
+history.listen(() => {
+  window.scrollTo(0, 0);
+});
+
+export default history;
