23cd659a683fb04405906ef0e02230dd0bd8e29a
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-03-15 17:54:45 +0545

    Add default value for API base

--- src/appConfig.js
+++ src/appConfig.js
@@ -4,7 +4,7 @@ import { version, name } from '../package.json';
  * Application wide configuration.
  */
 const appConfig = {
-  baseApiURI: process.env.REACT_APP_BASE_API_URI,
+  baseApiURI: process.env.REACT_APP_BASE_API_URI || 'https://hacker-news.firebaseio.com/v0/',
   devMode: process.env.NODE_ENV === 'development',
   isProdMode: process.env.NODE_ENV === 'production',
   app: {
