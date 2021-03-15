dfb7ee8923c44ab2672e8f5c0776fa992bcc3ff4
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-03-15 18:23:31 +0545

    Fix footer styling

--- src/components/Common/Footer/Footer.js
+++ src/components/Common/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './Footer.module.css';
+
 /**
  * Footer Component.
  *
@@ -8,43 +10,45 @@ import { Link } from 'react-router-dom';
  */
 function Footer() {
   return (
-    <div className="footer clearfix">
-      <div className="footer-line" />
+    <div className="pt-20 pb-20">
+      <div className={styles.lineBreak} />
 
-      <ul className="footer-links">
-        <li>
-          <Link to="/guidelines">Guidelines</Link>
-        </li>
-        <li>
-          <Link to="/faq">FAQ</Link>
-        </li>
-        <li>
-          <Link to="/support">Support</Link>
-        </li>
-        <li>
-          <Link to="/api">API</Link>
-        </li>
-        <li>
-          <Link to="/security">Security</Link>
-        </li>
-        <li>
-          <Link to="/lists">Lists</Link>
-        </li>
-        <li>
-          <Link to="/bookmarklet">Bookmarklet</Link>
-        </li>
-        <li>
-          <Link to="/legal">Legal</Link>
-        </li>
-        <li>
-          <Link to="/apply">Apply to YC</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div className="center mt-20 mb-20">
+        <ul className={`align-center ${styles.links}`}>
+          <li>
+            <Link to="/guidelines">Guidelines</Link>
+          </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
+          <li>
+            <Link to="/support">Support</Link>
+          </li>
+          <li>
+            <Link to="/api">API</Link>
+          </li>
+          <li>
+            <Link to="/security">Security</Link>
+          </li>
+          <li>
+            <Link to="/lists">Lists</Link>
+          </li>
+          <li>
+            <Link to="/bookmarklet">Bookmarklet</Link>
+          </li>
+          <li>
+            <Link to="/legal">Legal</Link>
+          </li>
+          <li>
+            <Link to="/apply">Apply to YC</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
 
-      <form action="#" className="user-form">
+      <form action="#" className={styles.searchContainer}>
         <label>Search:</label>
         <input type="text" />
       </form>
